5551493c01e5a7dddb546420b14e41bc7c824bf5
Yuri Niyazov <yuri.niyazov@gmail.com>
2014-09-06 16:59:40 -0700

    Part 2

---
+++ plot2.R
@@ -0,0 +1,7 @@
+initial <- read.table("household_power_consumption_subset.txt", header = TRUE, sep=";")
+png(filename = "plot2.png", width = 480, height = 480, units = "px", bg="transparent")
+x <- as.POSIXct(paste(initial$Date, initial$Time), format="%d/%m/%Y %H:%M:%S")
+y <- initial$Global_active_power
+plot(x, y, type="n", ylab="Global Active Power (kilowatts)")
+lines(x, y)
+dev.off()
