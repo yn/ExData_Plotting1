edab04058bd969288a08ad101ce0c457f033ab82
Yuri Niyazov <yuri.niyazov@gmail.com>
2014-09-06 16:42:48 -0700

    Add transparency

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
 initial <- read.table("household_power_consumption_subset.txt", header = TRUE, sep=";")
-png(filename = "plot1.png", width = 480, height = 480, units = "px")
+png(filename = "plot1.png", width = 480, height = 480, units = "px", bg="transparent")
 hist(initial$Global_active_power, col='red', main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
