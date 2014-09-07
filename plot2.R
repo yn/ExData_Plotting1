142f7bfc60a3860175ada10333626e02379d77b1
Yuri Niyazov <yuri.niyazov@gmail.com>
2014-09-06 22:25:26 -0700

    Using more R-esque expressions

--- plot2.R
+++ plot2.R
@@ -1,7 +1,8 @@
-initial <- read.table("household_power_consumption_subset.txt", header = TRUE, sep=";")
-png(filename = "plot2.png", width = 480, height = 480, units = "px", bg="transparent")
-x <- as.POSIXct(paste(initial$Date, initial$Time), format="%d/%m/%Y %H:%M:%S")
-y <- initial$Global_active_power
-plot(x, y, type="n", ylab="Global Active Power (kilowatts)", xlab="")
-lines(x, y)
-dev.off()
+with(read.table("household_power_consumption_subset.txt", header = TRUE, sep=";"), {
+  png(filename = "plot2.png", width = 480, height = 480, units = "px", bg="transparent")
+  x <- as.POSIXct(paste(Date, Time), format="%d/%m/%Y %H:%M:%S")
+  plot(x, Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+  dev.off()
+})
+
+
