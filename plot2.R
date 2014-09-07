7124e509377ee7b6d1f3bb02ce70a2eb99731480
Yuri Niyazov <yuri.niyazov@gmail.com>
2014-09-06 17:03:24 -0700

    No x label

--- plot2.R
+++ plot2.R
@@ -2,6 +2,6 @@ initial <- read.table("household_power_consumption_subset.txt", header = TRUE, s
 png(filename = "plot2.png", width = 480, height = 480, units = "px", bg="transparent")
 x <- as.POSIXct(paste(initial$Date, initial$Time), format="%d/%m/%Y %H:%M:%S")
 y <- initial$Global_active_power
-plot(x, y, type="n", ylab="Global Active Power (kilowatts)")
+plot(x, y, type="n", ylab="Global Active Power (kilowatts)", xlab="")
 lines(x, y)
 dev.off()
